746e3b8dc617f8160f9dac2018cf2149d0b2981c
meemanoj <meemanoj@Adminj-PC>
2019-06-11 17:46:32 +0530

    step#2 Add sessfact

--- src/main/java/com/app/config/AppConfig.java
+++ src/main/java/com/app/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
 public class AppConfig {
@@ -24,4 +25,9 @@ public class AppConfig {
 		return d;
 	}
 	
+	public LocalSessionFactoryBean bean()
+	{
+		LocalSessionFactoryBean l=new LocalSessionFactoryBean();
+		return l;
+	}
 }
